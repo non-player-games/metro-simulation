9f030c7989b58c1f2684d60fd5730000e1bf2f76
user <localuser@localhost>
2017-11-02 21:19:52 -0700

    Define new stations and lines

--- simulation.go
+++ simulation.go
@@ -10,13 +10,14 @@ type Location struct {
 type Station struct {
 	Location Location
 	Riders   []Rider
-    Name string
-    ID int
+	Name     string
+	ID       int
 }
 
 // Line represents the train line (a list of locations)
 type Line struct {
 	Stations []Station
+	Name     string
 }
 
 // Rider represents a single rider with its detination
@@ -32,27 +33,20 @@ type Train struct {
 }
 
 // Define Initial Stations Need 10
+var MAPLE_STATION = Station{Location: Location{1, 1}, Name: "Maple Station", ID: 0}
+var PINE_STATION = Station{Location: Location{5, 1}, Name: "Pine Station", ID: 1}
+var MAHOGANY_STATION = Station{Location: Location{8, 2}, Name: "Mahogany Station", ID: 2}
+var PALM_STATION = Station{Location: Location{14, 2}, Name: "Palm Station", ID: 3}
+var ASH_STATION = Station{Location: Location{5, 4}, Name: "Ash Station", ID: 4}
+var CEDAR_STATION = Station{Location: Location{14, 5}, Name: "Cedar Station", ID: 5}
+var REDWOOD_STATION = Station{Location: Location{2, 6}, Name: "Redwood Station", ID: 6}
+var ELM_STATION = Station{Location: Location{8, 4}, Name: "Elm Station", ID: 7}
+var HOLLY_STATION = Station{Location: Location{12, 6}, Name: "Holly Station", ID: 8}
+var OAK_STATION = Station{Location: Location{5, 5}, Name: "Oak Station", ID: 9}
 
-var MAPLE_STATION = Station { Location: Location{1,1}, Name: "Maple Station", ID: 0 }
-var PINE_STATION = Station { Location: Location{5,1}, Name: "Pine Station", ID: 1 }
-var mahogany_staition = Station { Location: Location{8,2}, Name: "Mahogany Station", ID: 2 }
-var Palm_station = Station { Location: Location{14,2}, Name: "Palm Station", ID: 3 }
-var ash_station = Station { Location: Location{5,4}, Name: "Ash Station", ID: 4 }
-var cedar_station = Station { Location: Location{14,5}, Name: "Cedar Station", ID: 5 }
-var redwood_station = Station { Location: Location{2,6}, Name: "Redwood Station", ID: 6 }
-var elm_station = Station { Location: Location{8,4}, Name: "Elm Station", ID: 7 }
-var holly_station = Station { Location: Location{12,6}, Name: "Holly Station", ID: 8 }
-var oak_station = Station { Location: Location{5,5}, Name: "Oak Station", ID: 9 }
-// const STATIONS [10]Station
-/*
-0 Maple Station, 1,1
-1 Pine Station 5,1
-2 Mahogany Station 8,2
-3 Palm Station 14,2
-4 Ash Station 5,4
-5 Cedar Station 14, 5
-6 Redwood Station 2,6
-7 Elm Station 8,4
-8 Holly Station 12, 6
-9 Oak Station 5,5
-*/
+// Define Lines
+var TOMATO_LINE = Line{[]Station{MAPLE_STATION, PINE_STATION, MAHOGANY_STATION}, "Tomato"}
+var AVOCADO_LINE = Line{[]Station{MAHOGANY_STATION, PALM_STATION, CEDAR_STATION}, "Avocado"}
+var BLUEBERRY_LINE = Line{[]Station{MAPLE_STATION, REDWOOD_STATION, OAK_STATION, ASH_STATION, PINE_STATION}, "Tomato"}
+var ORANGE_LINE = Line{[]Station{ASH_STATION, ELM_STATION, MAHOGANY_STATION}, "Orange"}
+var BANANA_LINE = Line{[]Station{ELM_STATION, HOLLY_STATION, CEDAR_STATION}, "Tomato"}
