6febab3549753e40900fc3c7d5837efecad15c95
Eric Liao <rcliao01@gmail.com>
2017-11-03 00:34:30 -0700

    refactor: add store package for data generation

--- simulation.go
+++ simulation.go
@@ -31,22 +31,3 @@ type Train struct {
 	Line           Line
 	Riders         []Rider
 }
-
-// Define Initial Stations Need 10
-var MAPLE_STATION = Station{Location: Location{1, 1}, Name: "Maple Station", ID: 0}
-var PINE_STATION = Station{Location: Location{5, 1}, Name: "Pine Station", ID: 1}
-var MAHOGANY_STATION = Station{Location: Location{8, 2}, Name: "Mahogany Station", ID: 2}
-var PALM_STATION = Station{Location: Location{14, 2}, Name: "Palm Station", ID: 3}
-var ASH_STATION = Station{Location: Location{5, 4}, Name: "Ash Station", ID: 4}
-var CEDAR_STATION = Station{Location: Location{14, 5}, Name: "Cedar Station", ID: 5}
-var REDWOOD_STATION = Station{Location: Location{2, 6}, Name: "Redwood Station", ID: 6}
-var ELM_STATION = Station{Location: Location{8, 4}, Name: "Elm Station", ID: 7}
-var HOLLY_STATION = Station{Location: Location{12, 6}, Name: "Holly Station", ID: 8}
-var OAK_STATION = Station{Location: Location{5, 5}, Name: "Oak Station", ID: 9}
-
-// Define Lines
-var TOMATO_LINE = Line{[]Station{MAPLE_STATION, PINE_STATION, MAHOGANY_STATION}, "Tomato"}
-var AVOCADO_LINE = Line{[]Station{MAHOGANY_STATION, PALM_STATION, CEDAR_STATION}, "Avocado"}
-var BLUEBERRY_LINE = Line{[]Station{MAPLE_STATION, REDWOOD_STATION, OAK_STATION, ASH_STATION, PINE_STATION}, "Blueberry"}
-var ORANGE_LINE = Line{[]Station{ASH_STATION, ELM_STATION, MAHOGANY_STATION}, "Orange"}
-var BANANA_LINE = Line{[]Station{ELM_STATION, HOLLY_STATION, CEDAR_STATION}, "Banana"}
---
+++ store/init.go
@@ -0,0 +1,118 @@
+package store
+
+import (
+	simulation "github.com/non-player-games/metro-simulation"
+	"github.com/rcliao/redux"
+)
+
+// Store is the central state management
+var Store redux.Store
+
+// Init initializes the default state
+func Init() {
+	state := make(map[string]interface{})
+	stations := map[string]simulation.Station{
+		"MAPLE_STATION": simulation.Station{
+			ID:       0,
+			Location: simulation.Location{X: 1, Y: 1},
+			Name:     "Maple Station",
+		},
+		"Pine Station": simulation.Station{
+			Location: simulation.Location{X: 5, Y: 1},
+			Name:     "Pine Station",
+			ID:       1,
+		},
+		"Mahogany Station": simulation.Station{
+			Location: simulation.Location{X: 8, Y: 2},
+			Name:     "Mahogany Station",
+			ID:       2,
+		},
+		"Palm Station": simulation.Station{
+			Location: simulation.Location{X: 14, Y: 2},
+			Name:     "Palm Station",
+			ID:       3,
+		},
+		"Ash Station": simulation.Station{
+			Location: simulation.Location{X: 5, Y: 4},
+			Name:     "Ash Station",
+			ID:       4,
+		},
+		"Cedar Station": simulation.Station{
+			Location: simulation.Location{X: 14, Y: 5},
+			Name:     "Cedar Station",
+			ID:       5,
+		},
+		"Redwood Station": simulation.Station{
+			Location: simulation.Location{X: 2, Y: 6},
+			Name:     "Redwood Station",
+			ID:       6,
+		},
+		"Elm Station": simulation.Station{
+			Location: simulation.Location{X: 8, Y: 4},
+			Name:     "Elm Station",
+			ID:       7,
+		},
+		"Holly Station": simulation.Station{
+			Location: simulation.Location{X: 12, Y: 6},
+			Name:     "Holly Station",
+			ID:       8,
+		},
+		"Oak Station": simulation.Station{
+			Location: simulation.Location{X: 5, Y: 5},
+			Name:     "Oak Station",
+			ID:       9,
+		},
+	}
+	state["stations"] = getStationSlice(stations)
+	state["lines"] = []simulation.Line{
+		simulation.Line{
+			Stations: []simulation.Station{
+				stations["MAPLE_STATION"],
+				stations["PINE_STATION"],
+				stations["MAHOGANY_STATION"],
+			},
+			Name: "Tomato",
+		},
+		simulation.Line{
+			Stations: []simulation.Station{
+				stations["MAHOGANY_STATION"],
+				stations["PALM_STATION"],
+				stations["CEDAR_STATION"],
+			},
+			Name: "Avocado",
+		},
+		simulation.Line{
+			Stations: []simulation.Station{
+				stations["MAPLE_STATION"],
+				stations["REDWOOD_STATION"],
+				stations["OAK_STATION"],
+				stations["ASH_STATION"],
+				stations["PINE_STATION"],
+			},
+			Name: "Blueberry",
+		},
+		simulation.Line{
+			Stations: []simulation.Station{
+				stations["ASH_STATION"],
+				stations["ELM_STATION"],
+				stations["MAHOGANY_STATION"],
+			},
+			Name: "Orange",
+		},
+		simulation.Line{
+			Stations: []simulation.Station{stations["ELM_STATION"], stations["HOLLY_STATION"], stations["CEDAR_STATION"]},
+			Name:     "Banana",
+		},
+	}
+	// TODO: add reducers later
+	reducers := []redux.Reducer{}
+	Store := redux.NewStore(state, reducers)
+}
+
+func getStationSlice(m map[string]simulation.Station) []Station {
+	result := []Station{}
+	for _, s := range m {
+		result = append(result, s)
+	}
+	return result
+}
