46f2f56e19015a38b2ae7ba51ef46196a6ff2c21
Eric Liao <rcliao01@gmail.com>
2017-11-03 20:32:37 -0700

    chore: add reducer skeleton

---
+++ store/reducers.go
@@ -0,0 +1,22 @@
+package store
+
+import "github.com/rcliao/redux"
+
+// RiderStationReducer simulates the rider showing up at station
+func RiderStationReducer(state redux.State, action redux.Action) redux.State {
+	// 1. generate a list of riders with their expected destination
+	// 2. Based on destination, we will put rider into a station in the same line
+	return state
+}
+
+// TrainStationReducer simulates running train in the same line
+func TrainStationReducer(state redux.State, action redux.Action) redux.State {
+	// for each train: move it to next location in the train line
+	return state
+}
+
+// RiderTrainReducer simulates rider deciding to hop on the train or not based on its destination
+func RiderTrainReducer(state redux.State, action redux.Action) redux.State {
+	// for each rider in the station with a train, decide if rider want to hop on the train
+	return state
+}
