72692dd9c2a7fac1c17cc0d17b44fdc041404a4f
Eric Liao <rcliao01@gmail.com>
2017-11-04 15:32:51 -0700

    feat: add vanilla table for visualization

--- web/handlers.go
+++ web/handlers.go
@@ -3,10 +3,19 @@ package web
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
 
+	simulation "github.com/non-player-games/metro-simulation"
 	"github.com/non-player-games/metro-simulation/store"
 )
+
+// Homepage is DTO sending to homepage
+type Homepage struct {
+	Trains   []simulation.Train
+	Stations []simulation.Station
+	Lines    []simulation.Line
+}
 
 // Hello says hello
 func Hello() http.HandlerFunc {
@@ -20,5 +29,27 @@ func CurrentState() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(store.Store.GetState())
+	})
+}
+
+// Assets serves the static assets (js & css)
+func Assets() http.Handler {
+	return http.StripPrefix("/assets", http.FileServer(http.Dir("./web/assets")))
+}
+
+// Index renders the index page for submitting SQL queries to test
+func Index() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("./web/templates/index.html")
+		if err != nil {
+			panic(err)
+		}
+		state := store.Store.GetState()
+		dto := Homepage{
+			Trains:   state["trains"].([]simulation.Train),
+			Stations: state["stations"].([]simulation.Station),
+			Lines:    state["lines"].([]simulation.Line),
+		}
+		t.Execute(w, dto)
 	})
 }
