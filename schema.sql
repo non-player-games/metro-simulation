18948033147ff82215c90612afcc1a3503478d54
Eric Liao <rcliao01@gmail.com>
2017-11-11 08:20:48 -0800

    feat: add line_tickets for line price
    Closes #6

--- schema.sql
+++ schema.sql
@@ -1,6 +1,21 @@
-CREATE TABLE rider_events (
+CREATE TABLE IF NOT EXISTS rider_events (
     action varchar(50) NOT NULL,
     station varchar(50) NOT NULL,
     line varchar(50) NOT NULL,
     timestamp timestamp DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS line_tickets (
+    line varchar(50) NOT NULL PRIMARY KEY,
+    ticket_price float NOT NULL DEFAULT 1.99
+);
+
+INSERT INTO line_tickets
+(line, ticket_price)
+VALUES
+("Tomato", 1.99),
+("Avocado", 2.99),
+("Blueberry", 1.99),
+("Orange", 1.49),
+("Banana", 2.49)
+;
